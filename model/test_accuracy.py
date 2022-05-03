e502de88eb6f8d12c87478902e1e727b47ff5116
Marco Fariselli <marco.fariselli@greenwaves-technologies.com>
2022-05-03 17:55:47 +0200

    ported to nntool

--- model/test_accuracy.py
+++ model/test_accuracy.py
@@ -1,21 +1,22 @@
-# You need to have sourced the SDK
-# beware of utils submodule because there is one in nntool
-import sys
-import os
-sys.path.insert(0, os.environ['NNTOOL_DIR'])
 import glob
 from PIL import Image
 import numpy as np
+from nntool.api import NNGraph
 
-# NNTOOL Modules
-from execution.graph_executer import GraphExecuter
-from execution.quantization_mode import QuantizationMode
-from interpreter.nntool_shell import NNToolShell
+quant_model_path = "model/cifar10_model_uint8.tflite"
+G = NNGraph.load_graph(quant_model_path, load_quantization=True)
+G.quantize(
+    statistics=None,
+    graph_options={
+        "ne16": False,
+        "hwc": False,
+    }
+)
+G.adjust_order()
+G.add_dimensions()
+G.fusions('scaled_match_group')
 
 dataset_files = glob.glob("samples/*")
-G = NNToolShell.get_graph_from_commands(['open BUILD_MODEL/cifar10_model_uint8.json'])
-executer = GraphExecuter(G, qrecs=G.quantization)
-
 for file in dataset_files:
 	print(file)
 	img = Image.open(file)
@@ -24,8 +25,7 @@ for file in dataset_files:
 	# Even in quantized mode nntool requires float data exactly as you trained the model -> [-1.0: 1.0]
 	input_tensor = input_tensor.astype(np.float32) / 128 - 1
 
-	outputs = executer.execute([input_tensor], qmode=QuantizationMode.all_dequantize(), silent=True)
+	outputs = G.execute([input_tensor], quantize=True, dequantize=True)
 	pred_class = np.argmax(outputs[-1][0])
 	confidence = outputs[-1][0][pred_class]
 	print(f"Predicted Class: {pred_class} with confidence: {confidence}")
-
